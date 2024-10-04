f2c97103eaed9b3acdfbefa4948788dcaea65319
kartik883 <rajputkartik8448@gmail.com>
2024-10-04 23:22:05 +0530

    use of marker in map

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -17,11 +17,38 @@ class _HomeScreenState extends State<HomeScreen> {
     //target: LatLng(37.42965233500664, -122.06737456962),
     zoom: 14.4746,
   );
+
+  // to mark the location
+  List<Marker> _marker =[];
+  List<Marker> _list =[
+    Marker(
+        markerId: MarkerId('1'),
+      position: LatLng(28.704060, 77.102493),
+      infoWindow: InfoWindow(
+        title: 'my current location'
+      ),
+    ),
+
+    Marker(
+      markerId: MarkerId('2'),
+      position: LatLng(28.696369, 77.091537),
+      infoWindow: InfoWindow(
+          title: 'Mangolpuri',
+      ),
+    ),
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _marker.addAll(_list);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
         initialCameraPosition: kgoogle,
+        markers: Set<Marker>.of(_marker),
         mapType: MapType.normal,
         onMapCreated: (GoogleMapController controller){
           _controller.complete(controller);
